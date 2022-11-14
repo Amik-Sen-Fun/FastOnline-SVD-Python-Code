942eac9661e8aa9ff35068e31c25213a7579f092
Amik Sen <90422219+Amik-Sen-Fun@users.noreply.github.com>
2022-11-14 13:27:36 +0530

    created venv

---
+++ code.py
@@ -0,0 +1,8 @@
+# a matrix X
+# X -> Decomposed into U,V and S, such that 
+#  U, V are prthogonal and S is a diagonal matrix and USVtr = X & UtrSV = X
+
+# Take m and n such that m>n
+m = 5
+n = 2
+x = 
